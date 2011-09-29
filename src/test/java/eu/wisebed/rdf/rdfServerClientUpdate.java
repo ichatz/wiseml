a2503d1bc8259b7eed43c26c6a7af90cc4cf3a3a
Your Name <theodori@ceid.upatras.gr>
2011-09-29 14:57:42 +0300

    modified pom for ru1

---
+++ src/test/java/eu/wisebed/rdf/rdfServerClientUpdate.java
@@ -0,0 +1,23 @@
+package eu.wisebed.rdf;
+
+
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+
+
+public class rdfServerClientUpdate {
+
+    public static void main (String args[])   {
+
+
+        QueryExecution qExec = QueryExecutionFactory.sparqlService("http://127.0.0.1:1281/info/query", "SELECT * {?s ?p ?o}") ;
+        ResultSet rs = qExec.execSelect() ;
+        System.out.println(">>"+ ResultSetFormatter.asXMLString(rs));
+
+
+
+
+      }
+}
