641357330b6c660289115e6b11c26f5792b61346
Orestis Akribopoulos <akribopo@cti.gr>
2011-11-07 23:19:17 +0200

    Implementing Event System for NodeReadings

---
+++ src/main/java/eu/wisebed/wisedb/listeners/LastNodeReadingListener.java
@@ -0,0 +1,37 @@
+package eu.wisebed.wisedb.listeners;
+
+import eu.wisebed.wisedb.model.NodeReading;
+import org.apache.log4j.Logger;
+import org.hibernate.event.SaveOrUpdateEvent;
+import org.hibernate.event.def.DefaultSaveOrUpdateEventListener;
+
+/**
+ * Extends the Default SaveOrUpdate Listener used by Hibernate for handling save-update events.
+ */
+public class LastNodeReadingListener extends DefaultSaveOrUpdateEventListener {
+
+    /**
+     * Static logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(LastNodeReadingListener.class);
+
+    /**
+     * Handle the given update event.
+     *
+     * @param event The update event to be handled.
+     */
+    @Override
+    public void onSaveOrUpdate(final SaveOrUpdateEvent event) {
+
+        //Ignores all events which are not instances of the NodeReading class.
+        if (event.getObject() instanceof NodeReading) {
+
+            final NodeReading thisReading = (NodeReading) event.getObject();
+
+            LOGGER.debug(new StringBuilder().append(thisReading.getNode().getId()).append(": ").append(thisReading.getCapability().getName()).toString());
+
+            //Forward the event to LastNodeReadingObservable
+            LastNodeReadingObservable.getInstance().addNodeReading(thisReading);
+        }
+    }
+}
---
+++ src/main/java/eu/wisebed/wisedb/listeners/LastNodeReadingObservable.java
@@ -0,0 +1,48 @@
+package eu.wisebed.wisedb.listeners;
+
+import eu.wisebed.wisedb.model.NodeReading;
+
+import java.util.Observable;
+
+/**
+ * Notifies observers with the last NodeReading
+ */
+public class LastNodeReadingObservable extends Observable {
+
+    /**
+     * static instance(ourInstance) initialized as null.
+     */
+    private static LastNodeReadingObservable ourInstance = null;
+
+    /**
+     * Private constructor suppresses generation of a (public) default constructor.
+     */
+    private LastNodeReadingObservable() {
+    }
+
+    /**
+     * LastNodeReadingObservable is loaded on the first execution of LastNodeReadingObservable.getInstance()
+     * or the first access to LastNodeReadingObservable.ourInstance, not before.
+     *
+     * @return ourInstance
+     */
+    public static LastNodeReadingObservable getInstance() {
+        synchronized (LastNodeReadingObservable.class) {
+            if (ourInstance == null) {
+                ourInstance = new LastNodeReadingObservable();
+            }
+        }
+        return ourInstance;
+    }
+
+    /**
+     * Notifies all of its observers.
+     *
+     * @param nodeReading the Last NodeReading
+     */
+    public void addNodeReading(final NodeReading nodeReading) {
+        this.setChanged();
+        this.notifyObservers(nodeReading);
+    }
+}
+
